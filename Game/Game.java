4b398f46415d08f400a6aaa818349107756b8403
ekruminis <e.kruminis@lancaster.ac.uk>
2018-01-25 15:20:49 +0000

    Added new class Game -> character + movement, options

--- Game/Game.java
+++ Game/Game.java
@@ -1,11 +1,363 @@
-public class Game implements Runnable {
-    // main game loop
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.function.*;
 
-    public void run(){
+import org.jsfml.system.*;
+import org.jsfml.window.*;
+import org.jsfml.window.event.*;
+import org.jsfml.graphics.*;
 
-        while(true){
+public class Game {
 
-        }
+  enum State{
+		MENU,GAME
+	}
+
+  State playerChoice = State.GAME;
+	public State getState(){
+		return playerChoice;
+	}
+
+  private String Title = "Let's Play!";
+  private static int fontSize = 48;
+  private int screenWidth = 1024;
+  private int screenHeight = 768;
+  private RenderWindow window = new RenderWindow();
+  public int xPosition;
+  public int yPosition;
+
+  private ArrayList<Actor> actors = new ArrayList<Actor>( );
+
+  private abstract class Actor {
+    Drawable obj;
+    BiConsumer<Float, Float> setPosition;
+
+    int x  = 0;	// Current X-coordinate
+    int y  = 0;	// Current Y-coordinate
+
+    //
+    // Render the object at its new position
+    //
+
+    void draw(RenderWindow w) {
+      w.draw(obj);
+    }
+  }
+
+  private class Image extends Actor {
+		private Sprite img;
+    private int xx;
+    private int yy;
+    private String ImageFile = "graphics/player/idle.png";
+    private int a;
+
+		public Image(int x, int y) {
+      xPosition = x;
+      yPosition = y;
+			//
+			// Load image/ texture
+			//
+			Texture imgTexture = new Texture( );
+			try {
+				imgTexture.loadFromFile(Paths.get(ImageFile));
+			} catch (IOException ex) {
+				ex.printStackTrace( );
+			}
+			imgTexture.setSmooth(true);
+
+			img = new Sprite(imgTexture);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(imgTexture.getSize()), 2));
+
+      img.setPosition(x,y);
+			//
+			// Store references to object and key methods
+			//
+			obj = img;
+			setPosition = img::setPosition;
+		}
+
+    public Texture changeImg(String ImageFile2) {
+      Texture imgTexture = new Texture( );
+			try {
+				imgTexture.loadFromFile(Paths.get(ImageFile2));
+			} catch (IOException ex) {
+				ex.printStackTrace( );
+			}
+			imgTexture.setSmooth(true);
+      return imgTexture;
     }
 
+    public Sprite image() {
+      return img;
+    }
+
+    public void moveLeft() {
+      xx = 0;
+      if(xPosition >=1)
+        xPosition = xPosition - 5;
+      Texture l = changeImg("graphics/player/leftWalk1.png");
+      img = new Sprite(l);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(l.getSize()), 2));
+
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void moveRight() {
+      xx = 0;
+      if(xPosition <= screenWidth)
+        xPosition = xPosition + 5;
+      Texture r1 = changeImg("graphics/player/rightWalk1.png");
+      img = new Sprite(r1);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(r1.getSize()), 2));
+
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void idle() {
+      Texture i = changeImg("graphics/player/idle.png");
+      img = new Sprite(i);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(i.getSize()), 2));
+
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void up() {
+      xx = 0;
+      yy = 0;
+      if(yPosition >= 1)
+        yPosition = yPosition - 10;
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void upLeft() {
+      xx = 0;
+      yy = 0;
+      if(yPosition >= 1 && xPosition >=1) {
+        yPosition = yPosition - 10;
+        xPosition = xPosition - 5;
+      }
+      Texture ul = changeImg("graphics/player/jumpLeft.png");
+      img = new Sprite(ul);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(ul.getSize()), 2));
+
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void upRight() {
+      xx = 0;
+      yy = 0;
+      if(yPosition >= 1 && xPosition <= screenWidth) {
+        yPosition = yPosition - 10;
+        xPosition = xPosition + 5;
+      }
+
+      Texture ur = changeImg("graphics/player/jumpRight.png");
+      img = new Sprite(ur);
+      img.setOrigin(Vector2f.div(
+					new Vector2f(ur.getSize()), 2));
+
+      img.setPosition(xPosition, yPosition);
+    }
+
+    public void down() {
+      xx = 0;
+      yy = 0;
+      if(yPosition <= screenHeight)
+        yPosition = yPosition + 10;
+      img.setPosition(xPosition, yPosition);
+    }
+	}
+
+  public void run(int width, int height) {
+    int screenWidth = width;
+    int screenHeight = height;
+
+    window.create(new VideoMode(screenWidth, screenHeight),
+                  Title,
+                  WindowStyle.DEFAULT);
+
+    window.setFramerateLimit(30);
+
+    Image player = new Image(300, 500);
+
+    while (playerChoice==State.GAME) {
+      // Clear the screen
+      window.clear(Color.WHITE);
+
+      // Move all the actors around
+      player.idle();
+
+      if (Keyboard.isKeyPressed (Keyboard.Key.A)) {
+        player.moveLeft();
+      }
+      if (Keyboard.isKeyPressed (Keyboard.Key.D)) {
+        player.moveRight();
+      }
+      if (Keyboard.isKeyPressed (Keyboard.Key.W)) {
+        player.up();
+      }
+      if (Keyboard.isKeyPressed (Keyboard.Key.S)) {
+        player.down();
+      }
+
+      if (Keyboard.isKeyPressed (Keyboard.Key.W) && Keyboard.isKeyPressed (Keyboard.Key.A)) {
+        player.upLeft();
+      }
+
+      if (Keyboard.isKeyPressed (Keyboard.Key.W) && Keyboard.isKeyPressed (Keyboard.Key.D)) {
+        player.upRight();
+      }
+
+      player.image().draw(window, RenderStates.DEFAULT);
+
+      // Update the display with any changes
+      window.display( );
+
+      // Handle any events
+      for (Event event : window.pollEvents( )) {
+        if (event.type == Event.Type.CLOSED) {
+          // the user pressed the close button
+          System.exit(0);
+        }
+
+        if (Keyboard.isKeyPressed (Keyboard.Key.ESCAPE)) {
+          OptionMenu om = new OptionMenu();
+          om.show();
+
+        }
+      }
+    }
+  }
+
+  private class OptionMenu {
+    public void show() {
+
+      int noItemsInMenu = 4;
+      Text[] text = new Text[noItemsInMenu];
+      int position = 0;
+
+      Texture texture1 = new Texture();
+
+      try {
+          //Try to load the texture from file "mario.jpg"//change your paths boys/girl"
+          if(screenWidth == 1024)
+            texture1.loadFromFile(Paths.get("titleimgsmall.png"));
+          else if(screenWidth == 1600)
+            texture1.loadFromFile(Paths.get("titleimgmid.png"));
+          else if(screenWidth == 1920)
+            texture1.loadFromFile(Paths.get("titleimgbig.png"));
+          //Texture was loaded successfully - retrieve and print size
+          Vector2i size = texture1.getSize();
+
+         // System.out.println("The texture is " + size.x + "x" + size.y);
+      } catch(IOException ex) {
+          //Ouch! something went wrong
+          ex.printStackTrace();
+      }
+      Sprite sprite = new Sprite (texture1);
+
+      RenderWindow window = new RenderWindow ();
+
+      window.create (new VideoMode (screenWidth, screenHeight),
+              Title,
+              WindowStyle.DEFAULT);
+      window.setFramerateLimit (30);
+
+      Font sansRegular = new Font ();
+      try {
+          sansRegular.loadFromFile (Paths.get ("FreeSans.ttf"));
+      } catch (IOException ex) {
+          ex.printStackTrace ();
+      }
+
+      text[0] = new Text ("1024x768", sansRegular, 32);
+      text[0].setStyle (Text.ITALIC);
+      text[0].setPosition (screenWidth/3, screenHeight-(screenHeight-200));
+      text[0].setColor (Color.RED);
+
+      text[1] = new Text ("1600x900", sansRegular, 32);
+      FloatRect text2bounds = text[1].getLocalBounds ();
+      text[1].setStyle (Text.ITALIC);
+      text[1].setOrigin (text2bounds.width / 2, text2bounds.height / 2);
+      text[1].setPosition (screenWidth/3, screenHeight-(screenHeight-350));
+      text[1].setColor (Color.YELLOW);
+
+      text[2] = new Text ("1920x1080", sansRegular, 32);
+      text[2].setStyle (Text.ITALIC);
+      text[2].setPosition (screenWidth/3, screenHeight-(screenHeight-500));
+      text[2].setColor (Color.YELLOW);
+
+      text[3] = new Text ("Exit", sansRegular, 32);
+      text[3].setStyle (Text.ITALIC);
+      text[3].setPosition (screenWidth/3, screenHeight-(screenHeight-650));
+      text[3].setColor (Color.YELLOW);
+
+//Main loop
+      while (window.isOpen ()) {
+          window.clear ();
+          window.draw(sprite);
+
+          for(int i=0;i<4;i++)
+          window.draw (text[i]);
+
+
+          window.display ();
+
+          for (Event event : window.pollEvents ()) {
+              if (event.type == Event.Type.CLOSED) {
+                  // the user pressed the close button
+                  window.close ();
+              }
+
+              if (Keyboard.isKeyPressed (Keyboard.Key.W)) {
+                  if (position-1 >=0) {
+                      text[position].setColor (Color.YELLOW);
+                      position--;
+                      text[position].setColor (Color.RED);
+                      break;
+                  }
+
+              }
+              if (Keyboard.isKeyPressed (Keyboard.Key.S)) {
+                  if (position+1 < noItemsInMenu) {
+                      text[position].setColor (Color.YELLOW);
+                      position++;
+                      text[position].setColor (Color.RED);
+                      break;
+                  }
+              }
+              if (Keyboard.isKeyPressed (Keyboard.Key.SPACE)) {
+                  if (position == 0) {
+                      screenWidth = 1024;
+                      screenHeight = 768;
+                      window.close();
+                      show();
+
+                  }  else if (position == 1) {
+                    screenWidth = 1600;
+                    screenHeight = 900;
+                    window.close();
+                    show();
+                  } else if (position == 2) {
+                    screenWidth = 1920;
+                    screenHeight = 1080;
+                    window.close();
+                    show();
+                  } else if (position == 3) {
+                    window.close();
+                    run(screenWidth, screenHeight);
+                  }
+              }
+
+          }
+      }
+    }
+  }
 }
--- Game/MainMenu.java
+++ Game/MainMenu.java
@@ -9,6 +9,7 @@ import org.jsfml.system.*;
 import org.jsfml.window.*;
 import org.jsfml.window.event.*;
 import org.jsfml.graphics.*;
+import org.jsfml.audio.*;
 
 class MainMenu {
 
@@ -20,11 +21,9 @@ class MainMenu {
     private static String JavaVersion =
             Runtime.class.getPackage( ).getImplementationVersion( );
     private static String JdkFontPath =
-            "C:\\Program Files\\Java\\jdk" + JavaVersion +
-                    "\\jre\\lib\\fonts\\";
+            "C:/Program Files (x86)/Java/jdk1.8.0_161/jre/lib/fonts/";
     private static String JreFontPath =
-            "C:\\Program Files\\Java\\jre" + JavaVersion +
-                    "\\lib\\fonts\\";
+            "C:/Program Files (x86)/Java/jre1.8.0_161/lib/fonts/";
 
     private static int fontSize     = 48;
     private static String FontFile  = "LucidaSansRegular.ttf";
@@ -32,11 +31,13 @@ class MainMenu {
 
     private static String Title   = "Hello SCC210!";
 
-
+    private RenderWindow window = new RenderWindow ();
     private String FontPath;	// Where fonts were found
     private int noItemsInMenu=3;
     private int position=0;
     private boolean status=true;
+    private int screenWidth = 1024;
+    private int screenHeight = 768;
     private class Message  {
         private Text text;
 
@@ -93,10 +94,15 @@ class MainMenu {
 
         try {
             //Try to load the texture from file "mario.jpg"//change your paths boys/girl"
-            texture1.loadFromFile(Paths.get("C:\\Users\\LukePc\\Desktop\\210\\src\\Mario.jpg"));
-
+            if(screenWidth == 1024)
+              texture1.loadFromFile(Paths.get("titleimgsmall.png"));
+            else if(screenWidth == 1600)
+              texture1.loadFromFile(Paths.get("titleimgmid.png"));
+            else if(screenWidth == 1920)
+              texture1.loadFromFile(Paths.get("titleimgbig.png"));
             //Texture was loaded successfully - retrieve and print size
             Vector2i size = texture1.getSize();
+
            // System.out.println("The texture is " + size.x + "x" + size.y);
         } catch(IOException ex) {
             //Ouch! something went wrong
@@ -106,9 +112,9 @@ class MainMenu {
         return sprite;
     }
 
-
-
     public void run ( ) {
+      position = 0;
+      noItemsInMenu = 3;
         Text[] text = new Text[noItemsInMenu];
         //
         // Check whether we're running from a JDK or JRE install
@@ -124,11 +130,12 @@ class MainMenu {
         } catch (IOException ex) {
             ex.printStackTrace ();
         }
+
         //
         // Create a window
         //
-        RenderWindow window = new RenderWindow ();
-        window.create (new VideoMode (1024, 768),
+        // RenderWindow window = new RenderWindow ();
+        window.create (new VideoMode (screenWidth, screenHeight),
                 Title,
                 WindowStyle.DEFAULT);
         window.setFramerateLimit (30);
@@ -137,26 +144,26 @@ class MainMenu {
         //Create some texts with different colors, sizes and styles
         text[0] = new Text ("Play", sansRegular, 32);
         text[0].setStyle (Text.ITALIC);
-        text[0].setPosition (400, 200);
+        text[0].setPosition (screenWidth/3, screenHeight-(screenHeight-200));
         text[0].setColor (Color.RED);
 
         text[1] = new Text ("Option", sansRegular, 32);
         FloatRect text2bounds = text[1].getLocalBounds ();
         text[1].setStyle (Text.ITALIC);
         text[1].setOrigin (text2bounds.width / 2, text2bounds.height / 2);
-        text[1].setPosition (420, 400);
+        text[1].setPosition (screenWidth/3, screenHeight-(screenHeight-350));
         text[1].setColor (Color.YELLOW);
 
         text[2] = new Text ("Exit", sansRegular, 32);
         text[2].setStyle (Text.ITALIC);
-        text[2].setPosition (380, 600);
+        text[2].setPosition (screenWidth/3, screenHeight-(screenHeight-500));
         text[2].setColor (Color.YELLOW);
 
 //Main loop
         while (window.isOpen ()) {
 
             window.clear ();
-            window.draw (loadTextures ());
+            window.draw (loadTextures());
 
             for(int i=0;i<3;i++)
             window.draw (text[i]);
@@ -189,23 +196,157 @@ class MainMenu {
                 }
                 if (Keyboard.isKeyPressed (Keyboard.Key.SPACE)) {
                     if (position == 0) {
-                        status = true;
-                        window.close ();
+                      window.close ();
+                      Game g = new Game( );
+                      g.run(getWidth(), getHeight());
+                    } else if (position == 1) {
+                      window.close();
+                      optionMenu();
 
-                    } else if (position == 2) {
-                        window.close ();
-
+                    }  else if (position == 2) {
+                        System.exit(0);
                     }
                 }
 
             }
         }
     }
+
+    public void optionMenu ( ) {
+        position = 0;
+        noItemsInMenu = 4;
+        Text[] text = new Text[noItemsInMenu];
+        //
+        // Check whether we're running from a JDK or JRE install
+        // ...and set FontPath appropriately.
+        //
+        if ((new File (JreFontPath)).exists ()) FontPath = JreFontPath;
+        else FontPath = JdkFontPath;
+
+        Font sansRegular = new Font ();
+        try {
+            sansRegular.loadFromFile (
+                    Paths.get (FontPath + FontFile));
+        } catch (IOException ex) {
+            ex.printStackTrace ();
+        }
+        //
+        // Create a window
+        //
+        // RenderWindow window = new RenderWindow ();
+        window.create (new VideoMode (screenWidth, screenHeight),
+                Title,
+                WindowStyle.DEFAULT);
+        window.setFramerateLimit (30);
+        // Avoid excessive updates
+
+        //Create some texts with different colors, sizes and styles
+        text[0] = new Text ("1024x768", sansRegular, 32);
+        text[0].setStyle (Text.ITALIC);
+        text[0].setPosition (screenWidth/3, screenHeight-(screenHeight-200));
+        text[0].setColor (Color.RED);
+
+        text[1] = new Text ("1600x900", sansRegular, 32);
+        FloatRect text2bounds = text[1].getLocalBounds ();
+        text[1].setStyle (Text.ITALIC);
+        text[1].setOrigin (text2bounds.width / 2, text2bounds.height / 2);
+        text[1].setPosition (screenWidth/3, screenHeight-(screenHeight-350));
+        text[1].setColor (Color.YELLOW);
+
+        text[2] = new Text ("1920x1080", sansRegular, 32);
+        text[2].setStyle (Text.ITALIC);
+        text[2].setPosition (screenWidth/3, screenHeight-(screenHeight-500));
+        text[2].setColor (Color.YELLOW);
+
+        text[3] = new Text ("Exit", sansRegular, 32);
+        text[3].setStyle (Text.ITALIC);
+        text[3].setPosition (screenWidth/3, screenHeight-(screenHeight-650));
+        text[3].setColor (Color.YELLOW);
+
+//Main loop
+        while (window.isOpen ()) {
+            window.clear ();
+            window.draw (loadTextures());
+
+            for(int i=0;i<4;i++)
+            window.draw (text[i]);
+
+
+            window.display ();
+
+            for (Event event : window.pollEvents ()) {
+                if (event.type == Event.Type.CLOSED) {
+                    // the user pressed the close button
+                    window.close ();
+                }
+
+                if (Keyboard.isKeyPressed (Keyboard.Key.W)) {
+                    if (position-1 >=0) {
+                        text[position].setColor (Color.YELLOW);
+                        position--;
+                        text[position].setColor (Color.RED);
+                        break;
+                    }
+
+                }
+                if (Keyboard.isKeyPressed (Keyboard.Key.S)) {
+                    if (position+1 < noItemsInMenu) {
+                        text[position].setColor (Color.YELLOW);
+                        position++;
+                        text[position].setColor (Color.RED);
+                        break;
+                    }
+                }
+                if (Keyboard.isKeyPressed (Keyboard.Key.SPACE)) {
+                    if (position == 0) {
+                        screenWidth = 1024;
+                        screenHeight = 768;
+                        window.close();
+                        optionMenu();
+
+                    }  else if (position == 1) {
+                      screenWidth = 1600;
+                      screenHeight = 900;
+                      window.close();
+                      optionMenu();
+                    } else if (position == 2) {
+                      screenWidth = 1920;
+                      screenHeight = 1080;
+                      window.close();
+                      optionMenu();
+                    } else if (position == 3) {
+                      window.close();
+                      window.close();
+                      run();
+                    }
+                }
+
+            }
+        }
+    }
+
     public boolean getStatus(){
         return status;
     }
 
+    public int getHeight() {
+      return screenHeight;
+    }
+
+    public int getWidth() {
+      return screenWidth;
+    }
+
     public static void main (String args[ ]) {
+        // Music titlesong = new Music();
+        // try {
+        //   titlesong.openFromFile(Paths.get("titlesong.ogg"));
+        // } catch(IOException ex) {
+        //   ex.printStackTrace();
+        // }
+        // titlesong.setLoop(true);
+        // titlesong.play();
+
         MainMenu x = new MainMenu ();
         x.run ();
 
